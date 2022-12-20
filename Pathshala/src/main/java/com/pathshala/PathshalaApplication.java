b4da331f25a4d1598da006b11256c69ea71cfd18
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-12-20 17:39:59 +0530

    Pathshala Structure has been ready

---
+++ Pathshala/src/main/java/com/pathshala/PathshalaApplication.java
@@ -0,0 +1,13 @@
+package com.pathshala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathshalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathshalaApplication.class, args);
+	}
+
+}
---
+++ Pathshala/src/test/java/com/pathshala/PathshalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathshala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathshalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
