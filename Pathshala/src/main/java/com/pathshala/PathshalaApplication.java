547a51bb309c2ebc66c30c5b6e52418b6ca2a678
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-12-21 14:26:02 +0530

    swagger implemented in application

--- Pathshala/src/main/java/com/pathshala/PathshalaApplication.java
+++ Pathshala/src/main/java/com/pathshala/PathshalaApplication.java
@@ -3,7 +3,10 @@ package com.pathshala;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class PathshalaApplication {
 
 	public static void main(String[] args) {
---
+++ Pathshala/src/main/java/com/pathshala/Swagger.java
@@ -0,0 +1,23 @@
+package com.pathshala;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configurable
+@EnableSwagger2
+public class Swagger {
+
+	@Bean
+	public Docket docket() {
+
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.pathshala")).build();
+
+	}
+}
+//http://localhost:8080/swagger-ui.html#/
